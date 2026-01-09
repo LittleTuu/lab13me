7594f1ace0e75c7cb110ba46017dcee2bccfb710
PronchiBoss <somsak0805144709@gmail.com>
2026-01-09 21:34:52 +0700

    cpmpletedJa

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,37 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[30][70]) {
+    for (int k = 0; k < 72; k++) cout << "-";
+    cout << endl;
+
+    for (int i = 0; i < 30; i++) {
+        cout << "|";
+        for (int j = 0; j < 70; j++) {
+            if (image[i][j]) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|" << endl;
+    }
+
+    for (int k = 0; k < 72; k++) cout << "-";
+    cout << endl;
+}
+
+void updateImage(bool image[30][70], int s, int x, int y) {
+    for (int i = 0; i < 30; i++) {
+        for (int j = 0; j < 70; j++) {
+            double dx = i - x;
+            double dy = j - y;
+            double distance = sqrt(dx*dx + dy*dy);
+            
+            if (distance <= (double)s - 1) {
+                image[i][j] = true;
+            }
+        }
+    }
+}
